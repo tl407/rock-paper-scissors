eb19b258692b04a71da16b415e000922a99cc18d
tl407 <79329177+tl407@users.noreply.github.com>
2024-07-22 19:48:39 -0500

    Add getHumanChoice() function with test

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ function getComputerChoice(){
 
 let val = getComputerChoice();
 console.log(val)
+
+function getHumanChoice(){
+    let choice = prompt("Choose one: rock, paper, scissors");
+    if(choice.toLowerCase() == "rock"){
+        console.log("rock")    
+    }
+    if(choice.toLowerCase() == "paper"){
+        console.log("paper")
+    }
+    if(choice.toLowerCase() == "scissors"){
+        console.log("scissors")
+    }
+    return choice;
+}
+
+let humanChoice = getHumanChoice();
+console.log(humanChoice)
