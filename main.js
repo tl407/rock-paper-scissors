34a048cf42747d42fc4d70570e5c88e58c0b0ae6
tl407 <79329177+tl407@users.noreply.github.com>
2024-07-22 19:37:09 -0500

    Add getComputerChoice() function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Hello World")
+
+function getComputerChoice(){
+    let num = Math.random()
+
+    if (num <= .3) return "Rock";
+    if (num <= .6) return "Paper";
+    return "Scissors";
+}
+
+let val = getComputerChoice();
+console.log(val)
